04785b4ce9eac6419b5f2b6e92313aa026ad47a1
DJSeokHo <djseokho@vip.qq.com>
2019-06-20 18:34:18 +0900

    add data class add enum class add interface class add delegate class

--- app/src/main/java/com/swein/androidkotlintool/framework/util/screen/ScreenUtil.kt
+++ app/src/main/java/com/swein/androidkotlintool/framework/util/screen/ScreenUtil.kt
@@ -1,17 +1,68 @@
 package com.swein.androidkotlintool.framework.util.screen
 
-import android.view.WindowManager
-import android.view.Window.FEATURE_NO_TITLE
 import android.app.Activity
-import android.content.Context
 import android.graphics.Color
 import android.os.Build
+import android.view.View
 import android.view.Window
+import android.view.WindowManager
 import com.swein.androidkotlintool.constants.Constants
 
 
 class ScreenUtil {
     companion object {
+
+        /**
+         * android full screen above 4.4
+         * @param activity activity
+         */
+        fun fullScreenBackwardCompatibility(activity: Activity) {
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+                return
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                val window = activity.window
+                val decorView = window.decorView
+
+                val option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                decorView.systemUiVisibility = option
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                window.statusBarColor = Color.TRANSPARENT
+            }
+            else {
+
+                val window = activity.window
+                val attributes = window.attributes
+                val flagTranslucentStatus = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                val flagTranslucentNavigation = WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+                attributes.flags = attributes.flags or flagTranslucentStatus
+                //                attributes.flags |= flagTranslucentNavigation;
+                window.attributes = attributes
+            }
+        }
+
+        /**
+         *
+         * android full screen above 5.0
+         *
+         * @param activity
+         */
+        fun fullScreen(activity: Activity) {
+            // full screen
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                activity.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            }
+        }
 
         /**
          *
