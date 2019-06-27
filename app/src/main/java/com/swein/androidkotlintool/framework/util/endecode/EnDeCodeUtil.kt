f5c692ddeafa293a2b683a0a2bd382c1cb9ac9db
DJSeokHo <djseokho@vip.qq.com>
2019-06-27 15:44:21 +0900

    add lots of utils

--- app/src/main/java/com/swein/androidkotlintool/framework/util/endecode/EnDeCodeUtil.kt
+++ app/src/main/java/com/swein/androidkotlintool/framework/util/endecode/EnDeCodeUtil.kt
@@ -5,10 +5,15 @@ import android.util.Base64
 import java.io.FileInputStream
 import java.io.IOException
 import java.io.InputStream
+import java.net.URLEncoder
 
 class EnDeCodeUtil {
 
     companion object {
+
+        fun chineseStringEncodeToUtf8(string: String): String {
+            return URLEncoder.encode(string, "UTF-8")
+        }
 
         fun fileToBase64(path: String): String? {
             if (TextUtils.isEmpty(path)) {
