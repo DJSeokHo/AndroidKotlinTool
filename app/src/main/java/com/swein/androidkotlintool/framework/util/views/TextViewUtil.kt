87cda2d3db758f2b66b63c62be486b05e0a596a1
DJSeokHo <djseokho@vip.qq.com>
2019-06-27 16:54:15 +0900

    add lots of utils

--- app/src/main/java/com/swein/androidkotlintool/framework/util/views/TextViewUtil.kt
+++ app/src/main/java/com/swein/androidkotlintool/framework/util/views/TextViewUtil.kt
@@ -4,11 +4,42 @@ import android.content.Context
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.RelativeSizeSpan
+import android.util.TypedValue
+import android.widget.LinearLayout
 import android.widget.TextView
+import com.swein.androidkotlintool.framework.util.dimension.DimensionUtil
 
 class TextViewUtil {
 
     companion object {
+
+        fun createWarpContentTextView(context: Context, paddingAll: Int, marginAll: Int, text: String, backgroundColor: Int, textSize: Float, textColor: Int): TextView {
+            val textView = TextView(context)
+            val params: LinearLayout.LayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+            params.setMargins(
+                DimensionUtil.dimension(paddingAll, context),
+                DimensionUtil.dimension(paddingAll, context),
+                DimensionUtil.dimension(paddingAll, context),
+                DimensionUtil.dimension(paddingAll, context)
+            )
+
+            textView.layoutParams = params
+
+            textView.setPadding(
+                DimensionUtil.dimension(marginAll, context),
+                DimensionUtil.dimension(marginAll, context),
+                DimensionUtil.dimension(marginAll, context),
+                DimensionUtil.dimension(marginAll, context)
+            )
+
+            textView.setBackgroundColor(backgroundColor)
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PT, textSize)
+            textView.setTextColor(textColor)
+            textView.text = text
+
+            return textView
+        }
 
         fun getSpannableString(context: Context, string: String, textView: TextView) {
             val spannableString = SpannableString(string)
