46bdb19b0dffad46adcd3a8d0f9800085f06b852
CodingWithCat <djseokho@vip.qq.com>
2021-09-14 16:31:12 +0900

    add system photo example

--- app/src/main/java/com/swein/androidkotlintool/main/examples/exoplayerexample/ExoPlayerExampleActivity.kt
+++ app/src/main/java/com/swein/androidkotlintool/main/examples/exoplayerexample/ExoPlayerExampleActivity.kt
@@ -1,6 +1,8 @@
 package com.swein.androidkotlintool.main.examples.exoplayerexample
 
+import android.app.Activity
 import android.content.res.Configuration
+import android.graphics.Color
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +10,8 @@ import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
@@ -17,9 +21,17 @@ import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import com.swein.androidkotlintool.R
+import android.os.Build
+import android.view.ViewGroup
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.swein.androidkotlintool.framework.util.log.ILog
+
 
 class ExoPlayerExampleActivity : AppCompatActivity() {
 
+    private lateinit var constraintLayoutRoot: ConstraintLayout
     private lateinit var exoPlayerView: PlayerView
 
     private lateinit var simpleExoPlayer: SimpleExoPlayer
@@ -35,11 +47,23 @@ class ExoPlayerExampleActivity : AppCompatActivity() {
         // <uses-permission android:name="android.permission.WAKE_LOCK" />
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
+        val rootView = window.decorView.rootView
+        ILog.debug("???", "${rootView.javaClass.kotlin.simpleName}")
+
         findView()
         initPlayer()
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { _, insets ->
+
+            ILog.debug("topInset", "${insets.systemWindowInsetTop}")
+            ILog.debug("bottomInset", "${insets.systemWindowInsetBottom}")
+
+            insets.consumeSystemWindowInsets()
+        }
     }
 
     private fun findView() {
+        constraintLayoutRoot = findViewById(R.id.constraintLayoutRoot)
         exoPlayerView = findViewById(R.id.exoPlayerView)
     }
 
@@ -49,13 +73,6 @@ class ExoPlayerExampleActivity : AppCompatActivity() {
         simpleExoPlayer.addListener(playerListener)
 
         exoPlayerView.player = simpleExoPlayer
-
-        exoPlayerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
 
         createMediaSource()
 
@@ -100,6 +117,42 @@ class ExoPlayerExampleActivity : AppCompatActivity() {
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
+
+        val constraintSet = ConstraintSet()
+        constraintSet.connect(exoPlayerView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0)
+        constraintSet.connect(exoPlayerView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0)
+        constraintSet.connect(exoPlayerView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 0)
+        constraintSet.connect(exoPlayerView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0)
+
+        constraintSet.applyTo(constraintLayoutRoot)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            actionBar?.hide()
+
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+        else {
+
+            actionBar?.show()
+
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    )
+
+            val layoutParams = exoPlayerView.layoutParams as ConstraintLayout.LayoutParams
+            layoutParams.dimensionRatio = "16:9"
+        }
+
+        window.decorView.requestLayout()
+
+
     }
 
     override fun onResume() {
