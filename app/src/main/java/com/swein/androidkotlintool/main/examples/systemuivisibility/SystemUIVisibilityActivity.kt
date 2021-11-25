349f38003fa1bc2333143c6a6217182ccbaa5d01
CodingWithCat <djseokho@vip.qq.com>
2021-11-25 15:38:57 +0900

    add shape able image example

--- app/src/main/java/com/swein/androidkotlintool/main/examples/systemuivisibility/SystemUIVisibilityActivity.kt
+++ app/src/main/java/com/swein/androidkotlintool/main/examples/systemuivisibility/SystemUIVisibilityActivity.kt
@@ -1,5 +1,6 @@
 package com.swein.androidkotlintool.main.examples.systemuivisibility
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.graphics.Color
 import android.os.Build
@@ -7,13 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.view.WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
 import android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.FrameLayout
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.swein.androidkotlintool.R
 import com.swein.androidkotlintool.framework.util.log.ILog
+import kotlin.random.Random
 
 class SystemUIVisibilityActivity : AppCompatActivity() {
 
@@ -24,112 +31,165 @@ class SystemUIVisibilityActivity : AppCompatActivity() {
         setContentView(R.layout.activity_system_uivisibility)
 
         findViewById<Button>(R.id.buttonVisibleStateBar).setOnClickListener {
-
-            setSystemStateBarVisible(this)
+            setStateBarVisible(this)
         }
-
         findViewById<Button>(R.id.buttonInVisibleStateBar).setOnClickListener {
-
-            setSystemStateBarInVisible(this)
+            setStateBarInVisible(this)
         }
-
-
-
         findViewById<Button>(R.id.buttonLightStateBar).setOnClickListener {
-
-            setSystemStateBarToLightTheme(this)
-            setSystemStatusBarColor(this, Color.CYAN)
+            setStateBarToLightTheme(this)
         }
-
         findViewById<Button>(R.id.buttonDarkStateBar).setOnClickListener {
+            setStateBarToDarkTheme(this)
+        }
+        findViewById<Button>(R.id.buttonStateBarColor).setOnClickListener {
 
-            setSystemStateBarToDarkTheme(this)
-            setSystemStatusBarColor(this, Color.DKGRAY)
+            setStatusBarColor(this, Color.argb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)))
         }
 
-    }
+        findViewById<Button>(R.id.buttonVisibleNavigationBar).setOnClickListener {
+            setNavigationBarVisible(this)
+        }
+        findViewById<Button>(R.id.buttonInVisibleNavigationBar).setOnClickListener {
+            setNavigationBarInVisible(this)
+        }
+        findViewById<Button>(R.id.buttonLightNavigationBar).setOnClickListener {
+            setNavigationBarToLightTheme(this)
+        }
+        findViewById<Button>(R.id.buttonDarkNavigationBar).setOnClickListener {
+            setNavigationBarToDarkTheme(this)
+        }
+        findViewById<Button>(R.id.buttonNavigationBarColor).setOnClickListener {
+            setNavigationBarColor(this, Color.argb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)))
+        }
 
 
-    fun initSystemBars() {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        findViewById<Button>(R.id.buttonEnableImmersive).setOnClickListener {
+            enableImmersiveMode(this)
+        }
+        findViewById<Button>(R.id.buttonDisableImmersive).setOnClickListener {
+            disableImmersiveMode(this)
+        }
 
-            val controller = window.decorView.windowInsetsController
 
-            // 设置状态栏反色
-            controller?.setSystemBarsAppearance(APPEARANCE_LIGHT_STATUS_BARS, APPEARANCE_LIGHT_STATUS_BARS)
-//            // 取消状态栏反色
-//            controller?.setSystemBarsAppearance(0, APPEARANCE_LIGHT_STATUS_BARS)
-//            // 设置导航栏反色
-//            controller?.setSystemBarsAppearance(APPEARANCE_LIGHT_NAVIGATION_BARS, APPEARANCE_LIGHT_NAVIGATION_BARS)
-//            // 取消导航栏反色
-//            controller?.setSystemBarsAppearance(0, APPEARANCE_LIGHT_NAVIGATION_BARS)
-//            // 同时设置状态栏和导航栏反色
-//            controller?.setSystemBarsAppearance((APPEARANCE_LIGHT_STATUS_BARS or APPEARANCE_LIGHT_NAVIGATION_BARS),
-//                (APPEARANCE_LIGHT_STATUS_BARS or APPEARANCE_LIGHT_NAVIGATION_BARS))
-//            // 同时取消状态栏和导航栏反色
-//            controller?.setSystemBarsAppearance(0, (APPEARANCE_LIGHT_STATUS_BARS or APPEARANCE_LIGHT_NAVIGATION_BARS))
-//            // 隐藏状态栏
-//            controller?.hide(WindowInsets.Type.statusBars())
-//            // 显示状态栏
-//            controller?.show(WindowInsets.Type.statusBars())
-//            // 隐藏导航栏
-//            controller?.hide(WindowInsets.Type.navigationBars())
-//            // 显示导航栏
-//            controller?.show(WindowInsets.Type.navigationBars())
-//            // 同时隐藏状态栏和导航栏
-//            controller?.hide(WindowInsets.Type.systemBars())
-//            // 同时隐藏状态栏和导航栏
-//            controller?.show(WindowInsets.Type.systemBars())
+        findViewById<Button>(R.id.buttonLayoutFullScreen).setOnClickListener {
+            layoutFullScreen(this)
+        }
+
+        findViewById<Button>(R.id.buttonLayoutNormalScreen).setOnClickListener {
+            layoutNormalScreen(this)
         }
     }
 
-    fun setSystemStateBarToLightTheme(activity: Activity) {
+    fun setStateBarVisible(activity: Activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            ViewCompat.getWindowInsetsController(window.decorView)?.isAppearanceLightStatusBars = true
+            activity.window.decorView.windowInsetsController?.show(WindowInsets.Type.statusBars())
         }
         else {
-            val lFlags = activity.window.decorView.systemUiVisibility
-            activity.window.decorView.systemUiVisibility = lFlags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            activity.window.decorView.systemUiVisibility = 0
         }
-
     }
 
-    fun setSystemStateBarToDarkTheme(activity: Activity) {
+    fun setStateBarInVisible(activity: Activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            ViewCompat.getWindowInsetsController(window.decorView)?.isAppearanceLightStatusBars = false
+            activity.window.decorView.windowInsetsController?.hide(WindowInsets.Type.statusBars())
         }
         else {
-            val lFlags = activity.window.decorView.systemUiVisibility
-            activity.window.decorView.systemUiVisibility = lFlags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         }
-
     }
 
-    fun setSystemStatusBarColor(activity: Activity, color: Int) {
+    fun setStateBarToLightTheme(activity: Activity) {
+        ViewCompat.getWindowInsetsController(activity.window.decorView)?.isAppearanceLightStatusBars = true
+    }
+
+    fun setStateBarToDarkTheme(activity: Activity) {
+        ViewCompat.getWindowInsetsController(activity.window.decorView)?.isAppearanceLightStatusBars = false
+    }
+
+    fun setStatusBarColor(activity: Activity, color: Int) {
         val window = activity.window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = color
     }
 
-
-
-    fun setSystemStateBarVisible(activity: Activity) {
+    fun setNavigationBarVisible(activity: Activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            activity.window.decorView.windowInsetsController?.show(WindowInsets.Type.statusBars())
+            activity.window.decorView.windowInsetsController?.show(WindowInsets.Type.navigationBars())
         }
         else {
+            activity.window.decorView.systemUiVisibility = 0
+        }
+    }
+
+    fun setNavigationBarInVisible(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            activity.window.decorView.windowInsetsController?.hide(WindowInsets.Type.navigationBars())
+        }
+        else {
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+    }
+
+
+    fun setNavigationBarToLightTheme(activity: Activity) {
+        ViewCompat.getWindowInsetsController(activity.window.decorView)?.isAppearanceLightNavigationBars = true
+    }
+
+    fun setNavigationBarToDarkTheme(activity: Activity) {
+        ViewCompat.getWindowInsetsController(activity.window.decorView)?.isAppearanceLightNavigationBars = false
+    }
+
+    fun setNavigationBarColor(activity: Activity, color: Int) {
+        val window = activity.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.navigationBarColor = color
+    }
+
+
+    fun enableImmersiveMode(activity: Activity) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            activity.window.setDecorFitsSystemWindows(false)
+            val controller = activity.window.insetsController
+            controller?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+        else {
+            activity.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+
+    }
+
+    fun disableImmersiveMode(activity: Activity) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            activity.window.setDecorFitsSystemWindows(true)
+            val controller = activity.window.insetsController
+            controller?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_DEFAULT
+            }
+        }
+        else {
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
 
         }
     }
 
-    fun setSystemStateBarInVisible(activity: Activity) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            activity.window.decorView.windowInsetsController?.hide(WindowInsets.Type.statusBars())
-        }
-        else {
+    fun layoutFullScreen(activity: Activity) {
+        activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
 
-        }
+    fun layoutNormalScreen(activity: Activity) {
+        activity.window.decorView.systemUiVisibility = 0
     }
 
 }
