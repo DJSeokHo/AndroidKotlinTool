3ded6eb8461d963976a5bcb9f37a357f925dd4c5
CodingWithCat <djseokho@vip.qq.com>
2022-01-12 16:08:32 +0900

    add string extension

--- app/src/main/java/com/swein/androidkotlintool/main/examples/broadcastexample/BroadcastExampleActivity.kt
+++ app/src/main/java/com/swein/androidkotlintool/main/examples/broadcastexample/BroadcastExampleActivity.kt
@@ -1,17 +1,49 @@
 package com.swein.androidkotlintool.main.examples.broadcastexample
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.swein.androidkotlintool.R
 
 class BroadcastExampleActivity : AppCompatActivity() {
+
+    private val broadcastReceiverExample = BroadcastReceiverExample()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_broadcast_example)
 
+        IntentFilter().apply {
+            addAction("com.swein.broadcast.passdata")
+            registerReceiver(broadcastReceiverExample, this)
+        }
+
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+
+            Intent().also { intent ->
+                intent.action = "com.swein.broadcast.passdata"
+                intent.putExtra("data", "coding with cat from data")
+                Bundle().also { bundle ->
+                    bundle.putString("value1", "coding")
+                    bundle.putString("value2", "with")
+                    bundle.putString("value3", "cat")
+
+                    intent.putExtra("bundle", bundle)
+                }
+
+                sendBroadcast(intent)
+            }
+
+        }
     }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(broadcastReceiverExample)
+    }
 
 }
--- app/src/main/java/com/swein/androidkotlintool/main/examples/broadcastexample/BroadcastReceiverExample.kt
+++ app/src/main/java/com/swein/androidkotlintool/main/examples/broadcastexample/BroadcastReceiverExample.kt
@@ -3,12 +3,26 @@ package com.swein.androidkotlintool.main.examples.broadcastexample
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import com.swein.androidkotlintool.main.examples.customintentexample.CustomIntentExampleActivity
 
 class BroadcastReceiverExample: BroadcastReceiver() {
 
+    // run on main thread
     override fun onReceive(context: Context, intent: Intent) {
 
+        intent.action?.let {
+            Log.d("BroadcastReceiverExample", "action $it")
+        }
 
+        Log.d("BroadcastReceiverExample", "data ${intent.getStringExtra("data")}")
 
+        intent.getBundleExtra("bundle")?.let {
+            Log.d("BroadcastReceiverExample", "${it.getString("value1", "")} ${it.getString("value2", "")} ${it.getString("value3", "")}")
+        }
+
+        Intent(context, CustomIntentExampleActivity::class.java).also {
+            context.startActivity(it)
+        }
     }
 }
